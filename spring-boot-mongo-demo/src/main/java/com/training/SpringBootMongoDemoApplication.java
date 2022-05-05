715620bb212300b95d8b62a93dabe65ee043224f
radha <radhavkrishna1994@gmail.com>
2022-05-05 14:38:31 +0530

    updated

--- spring-boot-mongo-demo/src/main/java/com/training/SpringBootMongoDemoApplication.java
+++ spring-boot-mongo-demo/src/main/java/com/training/SpringBootMongoDemoApplication.java
@@ -1,7 +1,13 @@
 package com.training;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.training.repo.CustomerRepo;
 
 @SpringBootApplication
 public class SpringBootMongoDemoApplication {
@@ -10,4 +16,16 @@ public class SpringBootMongoDemoApplication {
 		SpringApplication.run(SpringBootMongoDemoApplication.class, args);
 	}
 
+	@Autowired
+	private CustomerRepo customerRepo;
+	
+	@Bean
+	public CommandLineRunner getRunner(ApplicationContext ctx)
+	{
+		return args->{
+			System.out.println(customerRepo.findByName("sam"));
+			System.out.println(customerRepo.getByName("sam"));
+			System.out.println(customerRepo.getByNameEmail("sam", "sam@gmail.com"));
+		};
+	}
 }
--- spring-boot-mongo-demo/src/main/java/com/training/repo/CustomerRepo.java
+++ spring-boot-mongo-demo/src/main/java/com/training/repo/CustomerRepo.java
@@ -1,6 +1,9 @@
 package com.training.repo;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.training.model.Customer;
@@ -8,4 +11,12 @@ import com.training.model.Customer;
 @Repository
 public interface CustomerRepo extends MongoRepository<Customer, Long>{
 
+	public List<Customer> findByName(String name);
+	
+	@Query("{name:?0}")
+	public List<Customer> getByName(String name);
+	
+	@Query("{name: ?0, email: ?1}")
+	public List<Customer> getByNameEmail(String name,String email);
+	
 }
